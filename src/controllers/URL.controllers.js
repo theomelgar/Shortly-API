93d73b13a766fc1f3ba59a96bd90a8c821aba7a9
theomelgar <theomelgar@gmail.com>
2023-03-03 15:54:48 -0300

    fix: shorten

--- src/controllers/URL.controllers.js
+++ src/controllers/URL.controllers.js
@@ -3,7 +3,7 @@ import { nanoid } from "nanoid"
 
 export async function postShort(req, res) {
     const { url } = req.body
-    const shorten = nanoid(8 , url)
+    const shortUrl = nanoid(8, url)
     const { user } = res.locals
     try {
         const { rows } = await db.query(
@@ -11,10 +11,29 @@ export async function postShort(req, res) {
                 INSERT INTO links (url, "shortUrl", "userId" )
                 VALUES ($1, $2, $3)
                 RETURNING id
-            `,[url, shorten, user.id])
+            `, [url, shortUrl, user.id])
         res.status(201).send({
             id: rows[0].id,
-            shorten
+            shortUrl
+        })
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function getUrlById(req, res) {
+    const { id } = req.params
+
+    try {
+        const { rows: url, rowCount } = await db.query(
+            `
+                SELECT * FROM links WHERE id = $1
+            `, [id])
+        if( rowCount === 0 ) return res.sendStatus(404)
+        res.status(201).send({
+            id: url[0].id,
+            url: url[0].url,
+            shortUrl: url[0].shortUrl
         })
     } catch (error) {
         res.status(500).send(error.message)
--- src/routes/url.router.js
+++ src/routes/url.router.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
-import { postShort } from "../controllers/URL.controllers.js";
+import { getUrlById, postShort } from "../controllers/URL.controllers.js";
 import { validateSchema } from "../middlewares/validateSchemas.middleware.js";
 import { urlSchema } from "../schemas/url.schema.js";
+import auth from "../middlewares/auth.middleware.js";
 
 const urlRouter = Router()
 
-urlRouter.post('/urls/shorten', validateSchema(urlSchema), postShort)
-urlRouter.get('/urls/:id');
+urlRouter.post('/urls/shorten', auth, validateSchema(urlSchema), postShort)
+urlRouter.get('/urls/:id', getUrlById);
 urlRouter.get('/urls/open/:shortUrl');
 urlRouter.delete('/urls/:id');
 
