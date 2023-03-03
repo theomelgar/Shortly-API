32456401ba6bf262d286f0dc92edd58944709601
theomelgar <theomelgar@gmail.com>
2023-03-03 03:30:28 -0300

    fix: signin & signout middleware

--- src/middlewares/users.middleware.js
+++ src/middlewares/users.middleware.js
@@ -1,17 +1,33 @@
-import db from '../database/database.connection.js';
+import { db } from '../config/database.connection.js';
 import bcrypt from 'bcrypt';
 
-export async function signIn (req, res, next){
-    const {email, password} = req.body;
+export async function signUpMW(req, res, next) {
+    const { email } = req.body;
     try {
         const userExist = await db.query(
-            `SELECT * FROM users WHERE email=$1`,[email]
+            `SELECT * FROM users WHERE email=$1`, [email]
         )
-        if( userExist.rowCount <= 0){
+        if (userExist.rowCount > 0) {
+            return res.sendStatus(409)
+        }
+        res.locals.user = user
+        next()
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+export async function signInMW(req, res, next) {
+    const { email, password } = req.body;
+    try {
+        const userExist = await db.query(
+            `SELECT * FROM users WHERE email=$1`, [email]
+        )
+        if (userExist.rowCount <= 0) {
             return res.sendStatus(404)
         }
         const comparePassword = bcrypt.compareSync(password, userExist.rows[0].password)
-        if(!comparePassword){
+        if (!comparePassword) {
             return res.sendStatus(401)
         }
         res.locals.user = user
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,11 +2,13 @@ import { Router } from "express"
 import { signUp, signIn } from "../controllers/Users.controllers.js"
 import { validateSchema } from "../middlewares/validateSchemas.middleware.js"
 import { signUpSchema, loginSchema } from "../schemas/auth.schema.js"
+import { signInMW, signUpMW } from "../middlewares/users.middleware.js"
+
 
 const usersRouter = Router()
 
-usersRouter.post("/signup", validateSchema(signUpSchema), signUp)
-usersRouter.post("/signin", validateSchema(loginSchema), signIn)
+usersRouter.post("/signup", validateSchema(signUpSchema), signUpMW, signUp)
+usersRouter.post("/signin", validateSchema(loginSchema), signInMW, signIn)
 usersRouter.get('/users/me');
 usersRouter.get('/ranking');
 
